92fc5a5479c0b9baf23ac71695174ac56807d4c3
arda <ardaburc.arslanazi@elev.ga.ntig.se>
2020-11-05 13:44:44 +0100

    sss'

---
+++ dripping/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dripping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
